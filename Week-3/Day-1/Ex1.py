406c08c63685c80ec13559cf90c5d8fa071fd62e
Ruben Frisch <your rubenfrisch@icloud.com>
2026-03-08 20:14:49 +0200

    hey

---
+++ Week-3/Day-1/Ex1.py
@@ -0,0 +1,19 @@
+# Exercice 1:
+
+# What is data analysis?
+# Data analysis is the process of examining, cleaning, and interpreting data to find patterns, draw conclusions, and support decision-making.
+
+# Why is data analysis important in modern contexts?
+# It is important because everyone but also organizations generate large amounts of data and use analysis to make better decisions, improve efficiency, and predict future trends.
+
+# Three areas where data analysis is applied today:
+# Healthcare: analyzing patient data to improve treatments and track diseases
+# Sport: to follow to physical fitenss of the player and to prevent injuries
+# Marketing: studying customer behavior to improve products and advertising
+
+# Exercie 2:
+
+import pandas as pd
+
+sleep = pd.read_csv("Spend_Sleeping.csv")
+
---
+++ mila.py
@@ -0,0 +1,7 @@
+from collections import defaultdict  
+nums = defaultdict(int)  
+nums['one'] = 1  
+nums['two'] = 2
+nums['three'] = 3 
+print(nums['bbb'])
+    
